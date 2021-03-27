6e81688c577def2d2c75cdc3a05c43163c42e486
Joe Riebel <joeyriebel0@gmail.com>
2021-03-27 10:11:54 -0500

    step 1,2,3

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Ge a reference to the table body
+var tbody = d3.select("tbody")
+
+// Console.log the UFO data from data.js
+// console.log(tableData)
+
+// Step 1: Loop through 'data' and console.log each UFO object
+// tableData.forEach(function(ufoElement) {
+//     console.log(ufoElement);
+// });
+
+// Step 2: Use d3 to append one table row `tr` for each UFO object
+// Don't worry about adding cells or text yet, just t ry appending the `tr` elements.
+// tableData.forEach(function(uofElement) {
+//     console.log(uofElement);
+//     var row = tbody.append("tr");
+// });
+
+// Step 3: Use `Object.entries` to console.log each UFO element value
+tableData.forEach(function(uofElement) {
+    console.log(uofElement);
+    var row = tbody.append("tr");
+
+    Object.entries(uofElement).forEach(function([key, value]) {
+        console.log(key, value);
+    });
+});
